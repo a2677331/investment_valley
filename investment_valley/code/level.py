3b92d6a92c920a64b80f7ad5123addac00f8dbb2
Jian Zhong <zhongjian089@gmail.com>
2023-11-26 17:43:48 -0800

    update building collision range

--- investment_valley/code/level.py
+++ investment_valley/code/level.py
@@ -21,11 +21,11 @@ class Level:
         Generic((0,0), pygame.image.load("graphics/world/ground.png").convert_alpha(), self.all_sprites)
         
         # buildings' position and size for collision detection
-        building1_rect = pygame.Rect(130, 31, 179, 129)
-        building2_rect = pygame.Rect(553, 47, 197, 142)
-        building3_rect = pygame.Rect(987, 75, 206, 114)
-        building4_rect = pygame.Rect(261, 480, 117, 129)
-        building5_rect = pygame.Rect(1007, 500, 165, 125)
+        building1_rect = pygame.Rect(130, 31, 179, 100)
+        building2_rect = pygame.Rect(553, 47, 197, 105)
+        building3_rect = pygame.Rect(987, 75, 206, 85)
+        building4_rect = pygame.Rect(261, 480, 117, 92)
+        building5_rect = pygame.Rect(1007, 500, 165, 90)
         buidling_rects = [building1_rect, building2_rect, building3_rect, building4_rect, building5_rect]
         
         # player
@@ -35,3 +35,17 @@ class Level:
         self.display_surface.fill("black")
         self.all_sprites.draw(self.display_surface)
         self.all_sprites.update(dt) # update every sprite in setup()
+
+
+        ## For testing building collision
+        # building1_rect = pygame.Rect(130, 31, 179, 100)
+        # building2_rect = pygame.Rect(553, 47, 197, 105)
+        # building3_rect = pygame.Rect(987, 75, 206, 85)
+        # building4_rect = pygame.Rect(261, 480, 117, 92)
+        # building5_rect = pygame.Rect(1007, 500, 165, 90)
+
+        # pygame.draw.rect(self.display_surface, 'black', building1_rect)
+        # pygame.draw.rect(self.display_surface, 'black', building2_rect)
+        # pygame.draw.rect(self.display_surface, 'black', building3_rect)
+        # pygame.draw.rect(self.display_surface, 'black', building4_rect)
+        # pygame.draw.rect(self.display_surface, 'black', building5_rect)
