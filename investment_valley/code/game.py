c3a4cd7971c5a4d77349a785c2b6721c5d151afe
nbequettenorth1 <117419689+nbequettenorth1@users.noreply.github.com>
2023-12-02 14:17:02 -0500

    Update game.py

--- investment_valley/code/game.py
+++ investment_valley/code/game.py
@@ -33,6 +33,10 @@ class Game:
                 if self.level.run(dt, self.start_time)[0] <= 0: # if time is up, end the game
                     self.game_active = False
                     self.start_time = int(pygame.time.get_ticks() / 1000) # reset start time
+
+                    if self.level.player.in_stock_building:
+                        self.level.player.stock_menu()
+                        self.level.player.handle_stock_menu_input()
             else:
                 # show intro screen
                 show_intro_screen(self.screen, self.score)
